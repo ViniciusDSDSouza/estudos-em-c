5ed46f2f288d1184042e93c7fb3fcb15718310b9
ViniciusDSDSouza <viniciusdossantos547@gmail.com>
2025-02-21 01:27:41 -0300

    Concluindo Desafio Nivel Novato

---
+++ desafios-em-c/super-trunfo-novato/tempCodeRunnerFile.c
@@ -0,0 +1,4 @@
+    printf("Carta 1:\n\nEstado: %c\n\nCódigo: %c%s \n\nNome da Cidade: %s \n\nPopulação: %i \n\nÁrea: %.2f \n\nPIB: %.2f \n\nPontos Turísticos: %i\n\n", estadoCarta1, estadoCarta1, codigoCarta1, nomeCidadeCarta1, populacaoCarta1, areaCarta1, pibCarta1, pontosTuristicosCarta1);
+
+    // Carta 2
+    printf("Carta 2:\n\nEstado: %c\n\nCódigo: %c%s \n\nNome da Cidade: %s \n\nPopulação: %i \n\nÁrea: %.2f \n\nPIB: %.2f \n\nPontos Turísticos: %i\n\n", estadoCarta2, estadoCarta2, codigoCarta2, nomeCidadeCarta2, populacaoCarta2, areaCarta2, pibCarta2, pontosTuristicosCarta2);
