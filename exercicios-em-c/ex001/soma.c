828f9940a55019a8c49bc2b9813c9ed1d2e6affb
ViniciusDSDSouza <viniciusdossantos547@gmail.com>
2025-02-20 23:40:20 -0300

    Olá, mundo!

---
+++ exercicios-em-c/ex001/soma.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+    int n1,n2;
+
+    printf("Digite o 1° número: ");
+    scanf("%i", &n1);
+
+    printf("Digite o 2° número: ");
+    scanf("%i", &n2);
+
+    printf("Soma = %i", n1+n2);
+    
+    return 0;
+}
